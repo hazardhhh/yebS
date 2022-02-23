90b0bb0122cccb723d764175c399eb39437cb281
hazard <1145755268@qq.com>
2022-02-23 23:30:05 +0800

    权限组角色功能，菜单查询功能，菜单更新功能。

--- yeb-server/src/main/java/com/hhh/server/service/IMenuRoleService.java
+++ yeb-server/src/main/java/com/hhh/server/service/IMenuRoleService.java
@@ -2,6 +2,7 @@ package com.hhh.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hhh.server.pojo.MenuRole;
+import com.hhh.server.pojo.RespBean;
 
 /**
  * 服务类
@@ -9,4 +10,13 @@ import com.hhh.server.pojo.MenuRole;
  * @author hhh
  * @since 2022-01-19
  */
-public interface IMenuRoleService extends IService<MenuRole> {}
+public interface IMenuRoleService extends IService<MenuRole> {
+  /**
+   * 更新角色菜单
+   *
+   * @param rid
+   * @param mids
+   * @return
+   */
+  RespBean updateMenuRole(Integer rid, Integer[] mids);
+}
--- yeb-server/src/main/java/com/hhh/server/service/impl/MenuRoleServiceImpl.java
+++ yeb-server/src/main/java/com/hhh/server/service/impl/MenuRoleServiceImpl.java
@@ -1,10 +1,14 @@
 package com.hhh.server.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hhh.server.mapper.MenuRoleMapper;
 import com.hhh.server.pojo.MenuRole;
+import com.hhh.server.pojo.RespBean;
 import com.hhh.server.service.IMenuRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 服务实现类
@@ -14,4 +18,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MenuRoleServiceImpl extends ServiceImpl<MenuRoleMapper, MenuRole>
-    implements IMenuRoleService {}
+    implements IMenuRoleService {
+
+  @Autowired private MenuRoleMapper menuRoleMapper;
+
+  /**
+   * 更新角色菜单
+   *
+   * @param rid
+   * @param mids
+   * @return
+   */
+  @Override
+  @Transactional
+  public RespBean updateMenuRole(Integer rid, Integer[] mids) {
+    menuRoleMapper.delete(new QueryWrapper<MenuRole>().eq("rid", rid));
+    if (null == mids || 0 == mids.length) {
+      return RespBean.success("更新成功！");
+    }
+    Integer result = menuRoleMapper.insertRecord(rid, mids);
+    if (result == mids.length) {
+      return RespBean.success("更新成功！");
+    }
+    return RespBean.error("更新失败！");
+  }
+}
